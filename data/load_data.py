45a965c8b159325c5b526b9c708331fa539654e5
vbuiten <buiten@strw.leidenuniv.nl>
2021-10-04 11:42:26 +0200

    Set up project structure and added example of use script

---
+++ data/load_data.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def load_synth_spectra(regridded=True, small=False):
+    datapath = "/net/vdesk/data2/buiten/MRP2/pca-sdss-old/"
+    if regridded:
+        if small:
+            data = np.load(datapath+"gen_spectrum_regridded_array.npy")
+        else:
+            data = np.load(datapath+"gen_spectrum_regridded_big_array.npy")
+    else:
+        if small:
+            data = np.load(datapath+"gen_spectrum_nonregridded_array.npy")
+        else:
+            data = np.load(datapath+"gen_spectrum_nonregridded_big_array.npy")
+
+    wave_grid = data[0,:,0]
+    qso_cont = data[:,:,1]
+    qso_flux = data[:,:,2]
+
+    return wave_grid, qso_cont, qso_flux
+
+
+def split_data(attributes, targets, train_size=0.9, test_size=0.05):
+    rest_size = 1 - train_size
+    X_train, X_rest, y_train, y_rest = train_test_split(attributes, targets,\
+                                                        test_size=rest_size,\
+                                                        random_state=0)
+
+    test_size_of_rest = test_size/rest_size
+    X_valid, X_test, y_valid, y_test = train_test_split(X_rest, y_rest,\
+                                                        test_size=test_size_of_rest,
+                                                        random_state=0)
+
+    return X_train, X_valid, X_test, y_train, y_valid, y_test
---
+++ learning/learning.py
@@ -0,0 +1,95 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from sklearn.utils import shuffle
+import numpy as np
+from qso_fitting.models.utils.QuasarScaler import QuasarScaler
+from models.network import normalise, rescale_backward
+from utils.errorfuncs import MSE, corr_matrix_relresids
+
+def create_learners(parameters, learning_rate=0.1):
+    optimizer = torch.optim.AdamW(parameters, lr=learning_rate)
+    criterion = torch.nn.MSELoss(size_average=False)
+
+    return optimizer, criterion
+
+
+def train_scalers(wave_grid, X_train, y_train, floorval=0.05):
+
+    # calculate means and standard deviations
+    X_mean = np.mean(X_train, axis=0)
+    y_mean = np.mean(y_train, axis=0)
+    X_std = np.std(X_train, axis=0) + floorval * np.median(X_mean)
+    y_std = np.std(y_train, axis=0) + floorval * np.median(y_mean)
+
+    # create the scalers
+    scaler_X = QuasarScaler(wave_grid, X_mean, X_std)
+    scaler_y = QuasarScaler(wave_grid, y_mean, y_std)
+
+    return scaler_X, scaler_y
+
+
+def train_model(wave_grid, X_train, y_train, X_valid, y_valid, net,\
+                optimizer, criterion, batch_size=1000,\
+                num_epochs=1000, learning_rate=0.1, size_hidden=100):
+    '''Currently does not use a validation set!'''
+
+    # first train the scalers
+    scaler_X, scaler_y = train_scalers(wave_grid, X_train, y_train)
+
+    # use the scalers to normalise input and target
+    X_train_normed = normalise(scaler_X, X_train)
+    y_train_normed = normalise(scaler_y, y_train)
+
+    batch_no = len(X_train) // batch_size
+    #n_feature = X_train.shape[1]
+    #n_output = y_train.shape[1]
+
+    running_loss = np.zeros(num_epochs)
+
+    # setup the validation set for computing the MSE
+    X_valid_normed = normalise(scaler_X, X_valid)
+    y_valid_normed = normalise(scaler_y, y_valid)
+    input_valid = Variable(torch.FloatTensor(X_valid_normed.numpy()))
+    labels_valid = Variable(torch.FloatTensor(y_valid_normed.numpy()))
+    mse_loss_valid = np.zeros(num_epochs)
+
+    for epoch in range(num_epochs):
+        X_train_new, y_train_new = shuffle(X_train_normed, y_train_normed)
+
+        for i in range(batch_no):
+            start = i * batch_size
+            end = start + batch_size
+            inputs = Variable(torch.FloatTensor(X_train_new[start:end].numpy()))
+            labels = Variable(torch.FloatTensor(y_train_new[start:end].numpy()))
+
+            # set gradients to zero
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss[epoch] += loss.item()
+
+        print ("Epoch "+str(epoch+1)+"/"+str(num_epochs)+" completed.")
+        #print ("Loss: "+str(running_loss[epoch]))
+
+        # now use the validation set
+        output_valid = net(input_valid)
+        mse_loss_valid[epoch] = MSE(labels_valid.detach().numpy(), output_valid.detach().numpy())
+
+    return running_loss, mse_loss_valid, scaler_X, scaler_y
+
+
+def test_model(X_test, y_test, scaler_X, scaler_y, net):
+    '''Test the trained model by determining the MSE on the full model,
+    i.e. including the normalisation and back-conversion.'''
+
+    res_test = net.full_predict(X_test, scaler_X, scaler_y)
+    mse = MSE(y_test, res_test)
+    corr_matrix = corr_matrix_relresids(y_test, res_test, len(y_test))
+
+    return mse, corr_matrix
