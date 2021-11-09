3d3708910a8d04f09aecc337bf6b8718577244cc
vbuiten <buiten@strw.leidenuniv.nl>
2021-11-09 17:03:56 +0100

    Enabled the use of hand-fit continua + simulated forest + simulated noise for testing.

--- examples/train_linear_unet.py
+++ examples/train_linear_unet.py
@@ -8,6 +8,9 @@ import numpy as np
 import torch
 from models.network import normalise
 from data.load_data import load_synth_noisy_cont
+from qso_fitting.data.sdss.paris import read_paris_continua
+from scipy import interpolate
+from data.load_data import load_paris_spectra
 
 plt.rcParams["font.family"] = "serif"
 
@@ -28,13 +31,14 @@ flux_train, flux_valid, flux_test, cont_train, cont_valid, cont_test = split_dat
 n_feature = flux_train.shape[1]
 
 # set whether we want to smooth the input in the last skip connection
-smooth = False
+smooth = True
+no_final_skip = False
 
 # initialize the simple network and train with the QSOScalers
 unet = LinearUNet(n_feature, [100,200,300], activfunc="elu", operator="addition",\
-                  no_final_skip=True)
+                  no_final_skip=no_final_skip)
 optimizer, criterion = create_learners(unet.parameters(), learning_rate=0.001)
-trainer = UNetTrainer(unet, optimizer, criterion, num_epochs=300)
+trainer = UNetTrainer(unet, optimizer, criterion, num_epochs=200)
 trainer.train(wave_grid, flux_train, cont_train, flux_valid, cont_valid,\
               use_QSOScalers=True, smooth=smooth)
 
@@ -43,6 +47,23 @@ fig, ax = trainer.plot_loss(epoch_min=1)
 fig.show()
 
 # run some tests on the test set
+# use the hand-fit continua test set (half of it)
+#wave_hf, cont_hf, flux_hf, _, _ = read_paris_continua()
+
+# interpolate onto the hybrid grid
+#cont_hf_hybrid = interpolate.interp1d(wave_hf, cont_hf, kind="cubic", bounds_error=False,\
+#                                      fill_value="extrapolate", axis=1)(wave_grid)
+#flux_hf_hybrid = interpolate.interp1d(wave_hf, flux_hf, kind="cubic", bounds_error=False,\
+#                                      fill_value="extrapolate", axis=1)(wave_grid)
+
+# use the hand-fit continua + simulated forest + simulated noise
+wave_hf, cont_hf, flux_hf = load_paris_spectra(noise=True)
+
+# normalise the hand-fit spectra
+flux_test, cont_test = normalise_spectra(wave_hf, flux_hf, cont_hf)
+#flux_test, cont_test = normalise_spectra(wave_grid, flux_hf_hybrid, cont_hf_hybrid)
+wave_test = wave_grid
+
 # use the QSOScalers on input and output
 flux_test_scaled = trainer.scaler_X.forward(torch.FloatTensor(flux_test))
 cont_test_scaled = trainer.scaler_y.forward(torch.FloatTensor(cont_test))
@@ -50,7 +71,7 @@ cont_test_scaled = trainer.scaler_y.forward(torch.FloatTensor(cont_test))
 # plot the residuals vs wavelength
 stats = ResidualStatistics(flux_test, cont_test, scaler_flux=trainer.scaler_X,\
                            scaler_cont=trainer.scaler_y, net=unet, smooth=smooth)
-fig1, ax1 = stats.plot_means(wave_grid, show_std=False)
+fig1, ax1 = stats.plot_means(wave_test, show_std=False)
 fig1.show()
 
 # plot the residuals in a histogram
@@ -60,16 +81,17 @@ fig2.show()
 # plot the correlation matrix
 corrmat = CorrelationMatrix(flux_test, cont_test, trainer.scaler_X,\
                             trainer.scaler_y, unet, smooth=smooth)
-fig3, ax3 = corrmat.show(wave_grid)
+fig3, ax3 = corrmat.show(wave_test)
 
 # plot a random result
-testres = ModelResults(wave_grid, flux_test, cont_test, unet, scaler_flux=trainer.scaler_X,\
+testres = ModelResults(wave_test, flux_test, cont_test, unet, scaler_flux=trainer.scaler_X,\
                        scaler_cont=trainer.scaler_y, smooth=smooth)
 rand_indx = testres.random_index(4)
 testres.create_figure(figsize=(15,10))
 for i in range(len(rand_indx)):
     loc = int("22"+str(i+1))
-    testres.plot(rand_indx[i], subplotloc=loc)
+    ax = testres.plot(rand_indx[i], subplotloc=loc)
+testres.fig.suptitle("Test on Paris hand-fit spectra")
 
 testres.show_figure()
 #rand_indx = np.random.randint(len(flux_test))
