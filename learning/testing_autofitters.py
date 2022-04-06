78aa8460f18916620cdd2e6eaaa943262ed74d9a
vbuiten <buiten@strw.leidenuniv.nl>
2022-04-06 16:06:22 +0200

    Wrote classes for plotting example spectra and the correlation matrix for autofitter predictions

--- learning/testing_autofitters.py
+++ learning/testing_autofitters.py
@@ -33,16 +33,91 @@ class AutofitterPredictions:
         else:
             raise ValueError("Parameter 'model' should be either 'autofitter' or 'Qsmooth'.")
 
+        '''
         # convert flux and true continuum to numpy arrays for plotting purposes
-        self.flux = spectra.flux.detach().numpy()
-        self.cont = spectra.cont.detach().numpy()
+        flux = spectra.flux.detach().numpy()
+        cont = spectra.cont.detach().numpy()
+        '''
+
+        # get the normalised true continuum
+        norm_factor = self.flux_norm / self.spectra.flux
+        self.cont_norm = (norm_factor * self.spectra.cont).detach().numpy()
+
+        self.wave_grid = spectra.wave_grid
+
+
+class AutofitterPredictedSpectra(AutofitterPredictions):
+    def __init__(self, spectra, model="autofitter"):
+        super(AutofitterPredictedSpectra, self).__init__(spectra, model)
+
+    def random_index(self, size=1):
+
+        rand_indx = np.random.rand(0, len(self.spectra), size)
+
+        return rand_indx
+
+
+    def create_figure(self, figsize=(7,5), dpi=320):
+
+        self.fig = plt.figure(figsize=figsize, dpi=dpi)
+        self.axes = []
+
+        self.fig.suptitle("Model: "+self.model)
+
+
+    def plot(self, index, wave_lims=None, figsize=(7,5), dpi=320,
+             subplotloc=111, alpha=0.7, contpredcolor="darkred"):
+
+        try:
+            fig = self.fig
+        except:
+            fig = self.create_figure(figsize=figsize, dpi=dpi)
+
+        ax = fig.add_subplot(subplotloc)
+
+        if wave_lims is None:
+            grid = self.wave_grid
+            flux = self.flux_norm[index]
+            cont = self.cont_norm[index]
+            cont_pred = self.cont_norm_pred[index]
+
+        elif len(wave_lims) == 2:
+            sel = (self.wave_grid > wave_lims[0]) & (self.wave_grid < wave_lims[1])
+            grid = self.wave_grid[sel]
+            flux = self.flux_norm[sel][index]
+            cont = self.cont_norm[sel][index]
+            cont_pred = self.cont_norm_pred[sel][index]
+
+        else:
+            raise ValueError("Parameter 'wave_lims' must be an array-like object (wave_min, wave_max).")
+
+        ax.plot(grid, flux, alpha=alpha, lw=1., label="Mock spectrum")
+        ax.plot(grid, cont, alpha=alpha, lw=2., label="True continuum")
+        ax.plot(grid, cont_pred, alpha=alpha, lw=1, ls="--",
+                c=contpredcolor, label="Predicted continuum")
+
+        ax.set_xlabel(r"Rest-frame wavelength ($\AA$)")
+        ax.set_ylabel(r"Normalised flux")
+        ax.legend()
+        ax.grid()
+        ax.set_title(r"Results for test spectrum " + str(index + 1))
+
+        self.axes.append(ax)
+
+        return ax
+
+
+    def show_figure(self):
+
+        self.fig.tight_layout()
+        self.fig.show()
 
 
 class AutofitterRelResids(AutofitterPredictions):
     def __init__(self, spectra, model="autofitter"):
         super(AutofitterRelResids, self).__init__(spectra, model)
 
-        self.rel_resid = (self.cont - self.cont_pred) / self.cont
+        self.rel_resid = (self.cont_norm - self.cont_norm_pred) / self.cont_norm
         self.mean_spec = np.mean(self.rel_resid, axis=0)
         self.std_spec = np.std(self.rel_resid, axis=0)
         self.mad_std_spec = mad_std(self.rel_resid, axis=0)
@@ -107,3 +182,48 @@ class AutofitterResidualPlots(AutofitterRelResids):
         ax.set_title("Residuals relative to true continuum")
         ax.legend()
         return fig, ax
+
+
+class AutofitterCorrelationMatrix(AutofitterRelResids):
+    def __init__(self, spectra, model="autofitter"):
+        super(AutofitterCorrelationMatrix, self).__init__(spectra, model)
+
+        diff = self.rel_resid - self.mean_spec
+        covar_delta = 1 / (self.cont_norm.shape[0] - 1) * np.matmul(diff.T, diff)
+        corr_delta = covar_delta / np.sqrt(np.outer(np.diag(covar_delta), np.diag(covar_delta)))
+        self.matrix = corr_delta
+
+
+    def show(self, wave_lims=None):
+
+        self.fig, self.ax = plt.subplots(figsize=(7, 5), dpi=320)
+        self.im = self.ax.pcolormesh(self.wave_grid, self.wave_grid, self.matrix, cmap="bwr",
+                                     shading="nearest", vmin=-1.0, vmax=1.0)
+        self.ax.set_aspect("equal")
+        self.cbar = self.fig.colorbar(self.im, ax=self.ax, label="Residual correlation")
+        self.ax.set_xlabel("Rest-frame wavelength ($\AA$)")
+        self.ax.set_ylabel("Rest-frame wavelength ($\AA$)")
+        self.ax.set_title("Correlation matrix of residuals")
+
+        if wave_lims is None:
+            pass
+
+        elif len(wave_lims) == 2:
+            self.ax.set_xlim(*wave_lims)
+            self.ax.set_ylim(*wave_lims)
+
+        else:
+            raise ValueError("Parameter 'wave_lims' must be an array-like object (wave_min, wave_max).")
+
+        self.fig.show()
+
+        return self.fig, self.ax
+
+
+    def savefig(self, fullfilename):
+        '''Save the figure to a file with full path fullfilename. Only works if the figure is already created.'''
+
+        try:
+            self.fig.savefig(fullfilename)
+        except:
+            print ("Could not save a figure. Does it exist yet?")
