0b5de868ccf3efed91b41268d287d3d97ba28847
vbuiten <buiten@strw.leidenuniv.nl>
2022-06-07 17:26:43 +0200

    Removed embed() call from MedianScaler

--- utils/MedianScaler.py
+++ utils/MedianScaler.py
@@ -38,24 +38,18 @@ class MedianScaler:
     def forward(self, qso_spectrum):
 
         try:
+            if qso_spectrum.shape[1] == self.mean_spectrum.shape[0]:
+                spec_scaled = (qso_spectrum.to(self.device) - self.mean_spectrum) / self.median
 
-            try:
-                if qso_spectrum.shape[1] == self.mean_spectrum.shape[0]:
-                    spec_scaled = (qso_spectrum.to(self.device) - self.mean_spectrum) / self.median
-
-                else:
-                    spec_scaled = (qso_spectrum.to(self.device) - self.mean_spectrum[0]) / self.median[0]
-
-            except:
-                if qso_spectrum.shape[1] == self.mean_spectrum.shape[0]:
-                    spec_scaled = (qso_spectrum.to(self.device) - self.mean_spectrum.to(self.device)) / self.median.to(self.device)
-
-                else:
-                    spec_scaled = (qso_spectrum.to(self.device) - self.mean_spectrum[0].to(self.device)) / self.median[0].to(self.device)
+            else:
+                spec_scaled = (qso_spectrum.to(self.device) - self.mean_spectrum[0]) / self.median[0]
 
         except:
-            print ("Input shape:", qso_spectrum.shape)
-            embed()
+            if qso_spectrum.shape[1] == self.mean_spectrum.shape[0]:
+                spec_scaled = (qso_spectrum.to(self.device) - self.mean_spectrum.to(self.device)) / self.median.to(self.device)
+
+            else:
+                spec_scaled = (qso_spectrum.to(self.device) - self.mean_spectrum[0].to(self.device)) / self.median[0].to(self.device)
 
         return spec_scaled
 
