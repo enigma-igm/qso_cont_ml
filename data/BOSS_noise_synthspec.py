a1e3ec8fddc576abafea8d5633e961a364bb509a
vbuiten <buiten@strw.leidenuniv.nl>
2022-07-04 21:35:55 +0200

    Wrote first code for writing to hdf5 files

--- data/BOSS_noise_synthspec.py
+++ data/BOSS_noise_synthspec.py
@@ -1,9 +1,12 @@
-'''File for generating a training sample of QSO spectra with the
-Ly-alpha forest and with homoscedastic noise added in, saved on a uniform grid.'''
+'''File for generating a training sample of QSO spectra with empirical noise vectors from BOSS.
+The spectra are saved in an HDF5 file, on three grids: the hybrid grid, a uniform fine grid and a uniform coarse grid.
+The fine pixels have log(wav) spacing 1e-4; the coarse pixels have velocity width 500 km/s.
+No running-median smoothed spectrum is saved.'''
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+import h5py
 from dw_inference.sdss.utils import get_wave_grid
 from dw_inference.simulator.lognormal.lognormal_new import F_onorbe
 from dw_inference.simulator.utils import get_blu_red_wave_grid
@@ -36,8 +39,6 @@ wave_max = 2040.0
 wave_min = 1000.
 wave_max = 1970.
 fwhm = 131.4   # approximate BOSS FWHM (Smee+ 2013)
-#sampling = 2.0
-#dvpix = fwhm/sampling
 dloglam = 1.0e-4
 c_light = (const.c.to("km/s")).value
 dvpix = dloglam * c_light * np.log(10)
@@ -80,8 +81,7 @@ nsamp = 25000
 theta = Prox.sample_theta(nsamp)
 
 # simulate the continua and fluxes
-cont_prox, flux_prox = Prox.simulator(theta, replace=(nsamp > nskew),\
-                                      ivar=None)
+cont_prox, flux_prox = Prox.simulator(theta, replace=(nsamp > nskew), ivar=None)
 
 # normalise to one at 1280 \AA
 flux_norm, cont_norm = normalise_spectra(wave_rest, flux_prox, cont_prox)
@@ -102,56 +102,47 @@ for i in range(nsamp):
 
 flux_norm_noisy = flux_norm + noise_terms
 
-# smooth the flux before regridding
-flux_smooth = np.zeros(flux_norm_noisy.shape)
-for i, F in enumerate(flux_norm_noisy):
-    flux_smooth[i,:] = fast_running_median(F, window_size=20)
-
-# propagate the noise vector on the smoothed spectrum
-sigma_smooth = sigma_vectors/np.sqrt(20)
-ivar_smooth = 1/sigma_smooth**2
-
-# interpolate onto the hybrid grid
+# create a hybrid grid
 dvpix_red = 500.0
 gpm_norm = None
 
 # set the wavelength of the blue-red split
 wave_split = wave_1216
 
-wave_grid, dvpix_diff, ipix_blu, ipix_red = get_blu_red_wave_grid(wave_min, wave_max,\
-                                                                  wave_split, dvpix, dvpix_red)
+wave_grid, dvpix_diff, ipix_blu, ipix_red = get_blu_red_wave_grid(wave_min, wave_max, wave_split, dvpix, dvpix_red)
 
-cont_blu_red = interpolate.interp1d(wave_rest, cont_norm, kind="cubic", bounds_error=False,\
-                                    fill_value="extrapolate", axis=1)(wave_grid)
+# also create a fully coarse grid
+wave_coarse = get_wave_grid(wave_min, wave_max, dvpix_red)
 
-flux_blu_red, ivar_rebin, gpm_rebin, count_rebin = rebin_spectra(wave_grid,\
-                                                                 wave_rest,\
-                                                                 flux_norm_noisy,\
-                                                                 ivar_rand,\
-                                                                 gpm=gpm_norm)
-flux_smooth_blu_red, _, _, _ = rebin_spectra(wave_grid, wave_rest, flux_smooth,\
-                                             ivar_smooth, gpm=gpm_norm)
+# interpolate the continuum and rebin the absorption spectra & noise vectors
+cont_blu_red = interpolate.interp1d(wave_rest, cont_norm, kind="cubic", bounds_error=False, fill_value="extrapolate",
+                                    axis=1)(wave_grid)
+cont_coarse = interpolate.interp1d(wave_rest, cont_norm, kind="cubic", bounds_error=False, fill_value="extrapolate",
+                                   axis=1)(wave_coarse)
+
+flux_blu_red, ivar_rebin, gpm_rebin, count_rebin = rebin_spectra(wave_grid, wave_rest, flux_norm_noisy,
+                                                                 ivar_rand, gpm=gpm_norm)
+flux_coarse, ivar_coarse, gpm_coarse, count_coarse = rebin_spectra(wave_coarse, wave_rest, flux_norm_noisy,
+                                                                   ivar_rand, gpm=gpm_norm)
 
 # properly rebin the hybrid-grid noise vectors (i.e. with interpolation)
 ivar_rebin = interpBadPixels(wave_grid, ivar_rebin, gpm_rebin)
-#ivar_rebin = interpBadPixels(wave_grid, ivar_rebin, gpm=None)
+ivar_coarse = interpBadPixels(wave_coarse, ivar_coarse, gpm_coarse)
 
-# dirty fix to deal with zero-valued ivars
-#ivar_rebin[ivar_rebin == 0] = 1e-10
 sigma_rebin = 1 / np.sqrt(ivar_rebin)
+sigma_coarse = 1 / np.sqrt(ivar_coarse)
 
 print ("Number of negative ivar pixels:", np.sum(ivar_rebin <= 0))
+print ("Number of negative ivar pixels on coarse grid:", np.sum(ivar_rebin <= 0))
 
 # plot a random example and its noise vector
 
 idx = np.random.randint(0, len(cont_norm))
 
 fig, ax = plt.subplots(dpi=240)
-ax.plot(wave_grid, cont_blu_red[idx], alpha=0.7, label="Continuum")
-ax.plot(wave_grid, flux_blu_red[idx], alpha=0.5, label="Noisy spectrum", lw=.5)
-#ax.plot(wave_grid, flux_smooth_blu_red[idx], alpha=0.7, color="navy", ls="--",\
-#        label="Smoothed flux", lw=.5)
-ax.plot(wave_grid, sigma_rebin[idx], alpha=.7, color="darkred", lw=.5, label="Noise vector")
+ax.plot(wave_coarse, cont_coarse[idx], alpha=0.7, label="Continuum", c="tab:orange")
+ax.plot(wave_coarse, flux_coarse[idx], alpha=0.5, label="Noisy spectrum", lw=.5, c="tab:blue")
+ax.plot(wave_coarse, sigma_coarse[idx], alpha=.7, color="tab:green", lw=.5, label="Noise vector")
 ax.set_xlabel("Rest-frame wavelength ($\AA$)")
 ax.set_ylabel("Normalised flux")
 ax.legend()
@@ -159,45 +150,37 @@ ax.xaxis.set_minor_locator(AutoMinorLocator(5))
 ax.yaxis.set_minor_locator(AutoMinorLocator(5))
 ax.grid(which="major")
 ax.grid(which="minor", linewidth=.1, alpha=.3, color="grey")
-fig.suptitle("Noiseless continuum vs noisy spectrum with Ly-$\\alpha$ forest")
-ax.set_title("Empirical BOSS noise (spectrum {})".format(idx))
+fig.suptitle("Synthetic Spectrum {} with BOSS Noise".format(idx))
+ax.set_title("Coarse grid")
 fig.show()
 
-figpath = "/net/vdesk/data2/buiten/MRP2/misc-figures/hetsced-noise/BOSS-noise/"
-fig.savefig("{}synthspec-BOSSnoise-example{}.png".format(figpath, idx))
+#figpath = "/net/vdesk/data2/buiten/MRP2/misc-figures/hetsced-noise/BOSS-noise/"
+#fig.savefig("{}synthspec-BOSSnoise-example{}.png".format(figpath, idx))
 
 # plot the mean of the ivar across the spectrum
-ivar_mean_spec = np.mean(ivar_rebin, axis=0)
+ivar_mean_spec = np.mean(ivar_coarse, axis=0)
 fig2, ax2 = plt.subplots(dpi=240)
 ax2.plot(wave_grid, ivar_mean_spec)
 ax2.set_xlabel(r"Rest-frame wavelength ($\AA$)")
 ax2.set_ylabel(r"Normalised ivar (a.u.)")
 ax2.set_title("Mean of ivar noise across spectrum")
+fig2.suptitle("Mean of inverse variance")
 fig2.show()
 
-# save the grid, continuum and noisy continuum to an array
-savearray = np.zeros((nsamp, len(wave_rest), 5))
-savearray[:,:,0] = wave_rest
+# store everything in an hdf5 file
+filepath = "/net/vdesk/data2/buiten/MRP2/pca-sdss-old/"
+filename = "{}synthspec_BOSSlike_npca{}_z{}_large.hdf5".format(filepath, npca, z_qso)
+f = h5py.File(filename, "w")
 
-savearray_regridded = np.zeros((nsamp, len(wave_grid), 5))
-savearray_regridded[:,:,0] = wave_grid
+struct_arr_fine = np.zeros((3, nsamp, len(wave_rest)))
+struct_arr_coarse = np.zeros((3, nsamp, len(wave_coarse)))
+struct_arr_hybrid = np.zeros((3, nsamp, len(wave_grid)))
 
-for i in range(nsamp):
-    savearray[i,:,1] = cont_norm[i,:]
-    savearray[i,:,2] = flux_norm_noisy[i,:]
-    savearray[i,:,3] = flux_smooth[i,:]
-    savearray[i,:,4] = ivar_rand[i,:]
+#for i in range(3):
+#    struct_arr_fine[i] =
 
-    savearray_regridded[i,:,1] = cont_blu_red[i,:]
-    savearray_regridded[i,:,2] = flux_blu_red[i,:]
-    savearray_regridded[i,:,3] = flux_smooth_blu_red[i,:]
-    savearray_regridded[i,:,4] = ivar_rebin[i,:]
+dset_fine = f.create_dataset("uniform-fine",)
+dset_coarse = f.create_dataset("uniform-coarse")
+dset_hybrid = f.create_dataset("hybrid")
 
-savepath = "/net/vdesk/data2/buiten/MRP2/pca-sdss-old/"
-np.save(savepath+"forest_spectra_BOSSnoise_npca"+str(npca)+"BOSS-grid.npy",\
-        savearray)
-print ("Array saved.")
-
-np.save(savepath+"forest_spectra_BOSSnoise_npca"+str(npca)+"BOSS-regridded.npy",
-        savearray_regridded)
-print ("Regridded array saved.")
+f.close()
