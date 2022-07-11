704f9cc1346dd305815c4d5589efa146adde79e1
vbuiten <buiten@strw.leidenuniv.nl>
2022-07-11 16:50:50 +0200

    typo fix

--- models/conv_unet.py
+++ models/conv_unet.py
@@ -250,7 +250,7 @@ class UNet(nn.Module):
 
             print ("Shape of output before passing into final interpolation step:", out.shape)
 
-            weights = self.vel_weights.extend(out.size)
+            weights = self.vel_weights.expand(out.size)
 
             # then interpolate onto the coarse grid
             out = F.interpolate(out, scale_factor=weights, recompute_scale_factor=False)
