e8365612d062559503e8469f3542a6c90c45b932
vbuiten <buiten@strw.leidenuniv.nl>
2022-07-12 15:25:36 +0200

    Removed expand() call on interpolation weights

--- models/conv_unet.py
+++ models/conv_unet.py
@@ -249,7 +249,7 @@ class UNet(nn.Module):
 
             print ("Shape of output before passing into final interpolation step:", out.shape)
 
-            weights = self.vel_weights.expand(out.shape)
+            weights = self.vel_weights
 
             # then interpolate onto the coarse grid
             out = F.interpolate(out, scale_factor=weights, recompute_scale_factor=True,
