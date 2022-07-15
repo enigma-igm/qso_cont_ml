60cf4555e1ac9df7b183cd6a75150a14374acfe3
vbuiten <buiten@strw.leidenuniv.nl>
2022-07-15 11:10:54 +0200

    Removed coarse-grid scaler from new UNetTrainer

--- learning/learning_unet_new.py
+++ learning/learning_unet_new.py
@@ -46,15 +46,19 @@ class UNetTrainer:
         input_hybrid_scaler = torch.cat([cont_hybrid_unsq, ivar_hybrid_unsq, mean_trans_unsq], dim=1)
         mean_spec_hybrid = torch.mean(input_hybrid_scaler, dim=0)
 
+        '''TO DO: remove the coarse-grid scaler'''
+
+        '''
         # the coarse-grid scaler only needs the coarse-grid continuum
         cont_coarse_unsq = torch.unsqueeze(trainset.cont_coarse, dim=1)
         mean_cont_coarse = torch.mean(cont_coarse_unsq, dim=0)
+        '''
 
         # initialise the two scalers
         scaler_hybrid = MedianScaler(mean_spec_hybrid, floorval)
-        scaler_coarse = MedianScaler(mean_cont_coarse, floorval)
+        #scaler_coarse = MedianScaler(mean_cont_coarse, floorval)
 
-        return scaler_hybrid, scaler_coarse
+        return scaler_hybrid
 
 
     def train(self, trainset, validset, savefile, scaler_floorval=0.05):
@@ -73,7 +77,7 @@ class UNetTrainer:
         assert isinstance(validset, SynthSpectra)
 
         # train the hybrid-grid and coarse-grid scalers
-        self.scaler_hybrid, self.scaler_coarse = self.trainScalers(trainset, scaler_floorval)
+        self.scaler_hybrid = self.trainScalers(trainset, scaler_floorval)
 
         # check the dimensions of the scalers
         '''
@@ -92,8 +96,8 @@ class UNetTrainer:
         valid_loader = DataLoader(validset, batch_size=len(validset), shuffle=True)
 
         # set up velocity width weights
-        #Weights = WavWeights(trainset.wave_hybrid)
-        #weights_mse = Weights.weights_in_MSE.to(self.device)
+        Weights = WavWeights(trainset.wave_hybrid)
+        weights_mse = Weights.weights_in_MSE.to(self.device)
 
         self.wave_hybrid = trainset.wave_hybrid
         self.wave_coarse = trainset.wave_coarse
@@ -111,7 +115,6 @@ class UNetTrainer:
 
                 # scale the input and target output
                 flux_input_train = self.scaler_hybrid.forward(flux_input_raw)
-                true_cont_train = self.scaler_coarse.forward(true_cont_raw)
 
                 #print ("Number of NaN input values:", torch.sum(torch.isnan(flux_input_train)))
 
@@ -124,12 +127,11 @@ class UNetTrainer:
                 #print ("Number of NaN output values:", torch.sum(torch.isnan(outputs)))
 
                 # compute the weighted loss
-                outputs_real_rel = self.scaler_coarse.backward(outputs) / true_cont_raw
+                outputs_real_rel = self.scaler_hybrid.backward(outputs) / true_cont_raw
 
                 #print ("Number of NaN output values after descaling:", torch.sum(torch.isnan(outputs_real_rel)))
                 targets_rel = true_cont_raw / true_cont_raw
-                loss = self.criterion(outputs_real_rel, targets_rel)
-                #loss = self.criterion(outputs_real_rel * weights_mse, targets_rel * weights_mse)
+                loss = self.criterion(outputs_real_rel * weights_mse, targets_rel * weights_mse)
 
                 loss.backward()
 
@@ -152,11 +154,10 @@ class UNetTrainer:
                 valid_input_scaled = self.scaler_hybrid.forward(valid_input_raw)
 
                 valid_outputs = self.net(valid_input_scaled)
-                valid_outputs_real_rel = self.scaler_coarse.backward(valid_outputs) / valid_cont_raw
+                valid_outputs_real_rel = self.scaler_hybrid.backward(valid_outputs) / valid_cont_raw
                 valid_targets_rel = valid_cont_raw / valid_cont_raw
 
-                #validlossfunc = self.criterion(valid_outputs_real_rel * weights_mse, valid_targets_rel * weights_mse)
-                validlossfunc = self.criterion(valid_outputs_real_rel, valid_targets_rel)
+                validlossfunc = self.criterion(valid_outputs_real_rel * weights_mse, valid_targets_rel * weights_mse)
                 valid_loss[epoch] += validlossfunc.item()
 
                 #print ("Valid loss:", valid_loss[epoch])
@@ -176,7 +177,6 @@ class UNetTrainer:
                     "optimizer_state_dict": self.optimizer.state_dict(),
                     "valid_loss": valid_loss[epoch],
                     "scaler_hybrid": self.scaler_hybrid,
-                    "scaler_coarse": self.scaler_coarse,
                 }, savefile)
 
         # compute the loss per quasar once training is complete
