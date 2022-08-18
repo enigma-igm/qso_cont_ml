f29b3ddb1ba1203225f7f8d4f6fa2d89bfec65fe
vbuiten <buiten@strw.leidenuniv.nl>
2022-08-18 18:43:24 +0200

    Wrote function loadRedshiftLuminosityFile

--- data/boss_eda/load.py
+++ data/boss_eda/load.py
@@ -1,1 +1,27 @@
 '''Module for loading previously extracted BOSS DR14 redshifts and logLv values.'''
+
+import numpy as np
+import h5py
+import os
+
+def loadRedshiftLuminosityFile(savepath=None):
+    '''
+    Load the redshifts and log-luminosities at the Lyman limit of BOSS DR14 quasars.
+
+    @param savepath: str or NoneType
+    @return:
+    '''
+
+    if savepath is None:
+        savepath = os.getenv("SPECDB")
+    elif ~isinstance(savepath, str):
+        raise TypeError("Argument 'savepath' must be a string or None.")
+
+    f = h5py.File(savepath+"/luminosity-redshift-metadata.hdf5", "r")
+
+    redshifts = np.array(f["redshifts"])
+    logLv = np.array(f["logLv"])
+
+    f.close()
+
+    return redshifts, logLv
