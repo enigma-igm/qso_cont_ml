d5c99a5101dba7280b4ba96531780bb1a72cc909
vbuiten <buiten@strw.leidenuniv.nl>
2022-08-22 14:11:44 +0200

    Created file for exporting inferred P(z, logLv)

---
+++ data/boss_eda/export_distr.py
@@ -0,0 +1,6 @@
+'''Module for exporting the inferred/binned BOSS DR14 QSO distribution P(z, logLv).'''
+
+import numpy as np
+import h5py
+from visualisation import RedshiftHistogram, LuminosityHistogram
+
